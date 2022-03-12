1b894afd0d83780dfd43fade6d0aea7815eabb30
Daniel <dpangaribuan40@gmail.com>
2022-03-12 20:00:23 +0700

    update

--- controllers/report-controllers.js
+++ controllers/report-controllers.js
@@ -8,6 +8,7 @@ const getReportData = async (req, res) => {
 
 const getTotalSales = async (req, res) => {
     try {
+        const { start_date, end_date } = req.query;
         const GET_TOTAL_QUERY = `SELECT 
                                     ts.*, tp.*, tm.*, tr.*
                                 FROM
@@ -16,7 +17,8 @@ const getTotalSales = async (req, res) => {
                                     FROM
                                         transaction AS t
                                     WHERE
-                                        t.status_id = 7) AS ts,
+                                        t.status_id = 7 AND
+                                        t.created_at BETWEEN IFNULL(${start_date ? `'${start_date}'` : null}, t.created_at) AND IFNULL(${end_date ? `'${end_date}'` : null}, t.created_at)) AS ts,
                                     (SELECT 
                                         SUM(cd.quantity * (p.price - p.capital_price)) AS total_profit
                                     FROM
@@ -24,7 +26,8 @@ const getTotalSales = async (req, res) => {
                                     WHERE
                                         cd.product_id = p.id
                                             AND t.cart_id = cd.cart_id
-                                            AND t.status_id = 7) AS tp,
+                                            AND t.status_id = 7
+                                            AND t.created_at BETWEEN IFNULL(${start_date ? `'${start_date}'` : null}, t.created_at) AND IFNULL(${end_date ? `'${end_date}'` : null}, t.created_at)) AS tp,
                                     (SELECT 
                                         SUM(cd.quantity * p.price) AS total_revenue
                                     FROM
@@ -32,13 +35,15 @@ const getTotalSales = async (req, res) => {
                                     WHERE
                                         cd.product_id = p.id
                                             AND t.cart_id = cd.cart_id
-                                            AND t.status_id = 7) AS tr,
+                                            AND t.status_id = 7
+                                            AND t.created_at BETWEEN IFNULL(${start_date ? `'${start_date}'` : null}, t.created_at) AND IFNULL(${end_date ? `'${end_date}'` : null}, t.created_at)) AS tr,
                                     (SELECT 
                                         COUNT(*) AS total_member
                                     FROM
                                         user AS u
                                     WHERE
-                                        u.role_id = 1) AS tm;`;
+                                        u.role_id = 1
+                                        AND u.created_at BETWEEN IFNULL(${start_date ? `'${start_date}'` : null}, u.created_at) AND IFNULL(${end_date ? `'${end_date}'` : null}, u.created_at)) AS tm;`;
     
         const [ data_total ] = await db.execute(GET_TOTAL_QUERY);
 
@@ -57,8 +62,12 @@ const getTotalSales = async (req, res) => {
 
 const getProductSales = async (req, res) => {
     try {
+        const { start_date, end_date } = req.query;
         const GET_PRODUCT_SALES = `SELECT 
-                                        p.product_name AS x, CAST(SUM(cd.quantity) AS UNSIGNED) AS y
+                                        p.product_name AS product_name,
+                                        CAST(SUM(cd.quantity) AS UNSIGNED) AS quantity,
+                                        SUM(p.price * cd.quantity) AS revenue,
+                                        SUM((p.price - p.capital_price) * cd.quantity) AS net_income
                                     FROM
                                         transaction AS t,
                                         cart_detail AS cd,
@@ -67,7 +76,10 @@ const getProductSales = async (req, res) => {
                                         t.status_id = 7
                                             AND t.cart_id = cd.cart_id
                                             AND cd.product_id = p.id
-                                    GROUP BY p.product_name;`
+                                            AND t.created_at BETWEEN IFNULL(${start_date ? `'${start_date}'` : null}, t.created_at) AND IFNULL(${end_date ? `'${end_date}'` : null}, t.created_at)
+                                    GROUP BY product_name
+                                    ORDER BY quantity DESC
+                                    LIMIT 3;`
         
         const [ data_product_sales ] = await db.execute(GET_PRODUCT_SALES);
 
@@ -85,21 +97,84 @@ const getProductSales = async (req, res) => {
 
 const getIncomeStatement = async (req, res) => {
     try {
+        const { start_date, end_date } = req.query;
         const GET_INCOME_STATEMENT = `SELECT 
                                         SUM(cd.quantity * p.price) AS revenue,
                                         SUM(cd.quantity * (p.price - p.capital_price)) AS net_income,
                                         ROUND(((SUM(cd.quantity * p.price) - SUM(cd.quantity * p.capital_price)) / SUM(cd.quantity * p.price) * 100), 2) AS profit_margin,
-                                        CONCAT(MONTH(t.created_at), '/', YEAR(t.created_at)) AS month
+                                        CONCAT(YEAR(t.created_at), '/', MONTH(t.created_at), '/', '01') AS time
                                     FROM
                                         transaction AS t, cart_detail AS cd, product AS p
                                     WHERE 
                                         cd.product_id = p.id
                                             AND t.cart_id = cd.cart_id
                                             AND t.status_id = 7
-                                    GROUP BY month;`;
+                                            AND t.created_at BETWEEN IFNULL(${start_date ? `'${start_date}'` : null}, t.created_at) AND IFNULL(${end_date ? `'${end_date}'` : null}, t.created_at)
+                                    GROUP BY time
+                                    ORDER BY time ASC;`;
         const [ get_income_statement ] = await db.execute(GET_INCOME_STATEMENT);
 
         const data = get_income_statement;
+        res.status(200).send(new utils.CreateRespond(
+            200,
+            'Success',
+            data
+        ));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const numberOfSales = async (req, res) => {
+    try {
+        const { start_date, end_date } = req.query;
+        const GET_NUMBER_SALES = `SELECT 
+                                        p.product_name AS product_name,
+                                        CAST(SUM(cd.quantity) AS UNSIGNED) AS quantity
+                                    FROM
+                                        transaction AS t,
+                                        cart_detail AS cd,
+                                        product AS p
+                                    WHERE
+                                        t.status_id = 7
+                                            AND t.cart_id = cd.cart_id
+                                            AND cd.product_id = p.id
+                                            AND t.created_at BETWEEN IFNULL(${start_date ? `'${start_date}'` : null}, t.created_at) AND IFNULL(${end_date ? `'${end_date}'` : null}, t.created_at)
+                                    GROUP BY product_name
+                                    ORDER BY quantity DESC;`;
+        
+        const [ get_number_sales ] = await db.execute(GET_NUMBER_SALES);
+        const data = get_number_sales;
+        res.status(200).send(new utils.CreateRespond(
+            200,
+            'Success',
+            data
+        ));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const getCostReport = async (req, res) => {
+    try {
+        const { start_date, end_date } = req.query;
+        const GET_COST = `SELECT 
+                            p.product_name AS product_name,
+                            CAST(SUM(cd.quantity) AS UNSIGNED) AS quantity
+                        FROM
+                            transaction AS t,
+                            cart_detail AS cd,
+                            product AS p
+                        WHERE
+                            t.status_id = 7
+                                AND t.cart_id = cd.cart_id
+                                AND cd.product_id = p.id
+                                AND t.created_at BETWEEN IFNULL(${start_date ? `'${start_date}'` : null}, t.created_at) AND IFNULL(${end_date ? `'${end_date}'` : null}, t.created_at)
+                        GROUP BY product_name
+                        ORDER BY quantity DESC;`;
+        
+        const [ get_cost ] = await db.execute(GET_COST);
+        const data = get_cost;
         res.status(200).send(new utils.CreateRespond(
             200,
             'Success',
@@ -114,5 +189,7 @@ module.exports = {
     getReportData,
     getTotalSales,
     getProductSales,
-    getIncomeStatement
+    getIncomeStatement,
+    numberOfSales,
+    getCostReport
 }
--- routes/report-routes.js
+++ routes/report-routes.js
@@ -6,5 +6,6 @@ router.get('/report', report.getReportData);
 router.get('/reportTotalData', report.getTotalSales);
 router.get('/reportProductSales', report.getProductSales);
 router.get('/reportIncomeStatement', report.getIncomeStatement);
+router.get('/numberOfSales', report.numberOfSales);
 
 module.exports = router;
